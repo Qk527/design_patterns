f2ff4e97742a3f4308900cbc620ca05944c9ee42
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-04-16 09:29:07 -0300

    Update product.py

--- iFactory/product.py
+++ iFactory/product.py
@@ -2,4 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class Vehicle(ABC):
+    @abstractmethod
+    def deliver(self):
+        pass
+
+class Car(Vehicle):
+    def __init__(self, model):
+        self._model = model
     
+    def deliver(self):
+        return f"Car {self._model} delivered"
+
+class Truck(Vehicle):
+    def __init__(self, model):
+        self._model = model
+
+    def deliver(self):
+        return f"Truck {self._model} delivered"
