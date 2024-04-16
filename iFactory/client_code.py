6bb07b4e7c1c695966bf50edc0aee11596cd5fee
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-04-16 15:53:26 -0300

    Create client_code.py

---
+++ iFactory/client_code.py
@@ -0,0 +1,6 @@
+from concrete_factory import CarFactory, TruckFactory
+
+def client_vehicle_code(factory, vehicle_type):
+  vehicle = factory.get_vehicle(vehicle_type)
+  print(vehicle.delivery())
+  
