fcbfc5956ae86ffbe4c85a95b80dbfc49cf3a780
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-04-24 12:05:10 -0300

    Create main.py

---
+++ Builder/main.py
@@ -0,0 +1,7 @@
+from cook import Cook
+from pizza_builder import MargheritaBuilder
+
+cook = Cook()
+margherita_builder = MargheritaBuilder()
+pizza = cook.make_pizza(margherita_builder)
+print(pizza)
