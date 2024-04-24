761be417a6e3da42fe9f4a4a553f502dafb49bb7
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-04-24 11:51:27 -0300

    Create cook.py

---
+++ Builder/cook.py
@@ -0,0 +1,9 @@
+#este es el opcional
+from pizza_builder import MargheritaBuilder
+
+class Cook:
+  def make_pizza(self, builder):
+    builder.set_dough()
+    builder.set_sauce()
+    builder.set_topping()
+    return builder.pizza
