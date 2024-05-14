2fdb3fa706b5bd496e368d540920a05aba6eb411
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-05-14 14:40:58 -0300

    Update main.py

--- Adapter/main.py
+++ Adapter/main.py
@@ -9,6 +9,10 @@ if __name__ == '__main__':
   #Json handler
   json_handler = JsonDataHandler()
   json_file_path = 'data/sample_data.json'
+  load_and_display_data(json_handler, json_file_path)
 
   #csv handler with adapter
   csv_handler = CsvDataHandler()
+  csv_file_path = 'data/sample_data.csv'
+  csv_adapter = CsvToJsonAdapter(csv_hanlder)
+  load_and_display_data(csv_adapter, csv_file_path)
