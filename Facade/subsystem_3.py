bb5b3f4e2db82789fd349fee5b34c131fba32e3a
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-06-04 14:25:59 -0300

    Create subsystem_3.py

---
+++ Facade/subsystem_3.py
@@ -0,0 +1,7 @@
+# Sistema de entregas
+
+class Shiping:
+  # verifica el estado de entrega de un producto
+  def check_shipping(self, product_id):
+    print(f'Checking {product_id} in shipping'})
+    return True
