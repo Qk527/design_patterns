aab352e9b72c001c0fe5d2add899e60c9ae3966f
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-06-12 12:08:41 -0300

    Create leaf.py

---
+++ Composite/leaf.py
@@ -0,0 +1,9 @@
+# Obj individuales sin hijos
+from component import FileSystemComponent
+
+class File(FileSystemComponent):
+  def __init__(self, name):
+    self.name = name
+
+  def show_details(self):
+    print(f"File: {self.name}")
