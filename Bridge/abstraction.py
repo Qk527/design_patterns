9dfbd276b7f6dcdecd77981342f3520b5c6297dd
Julieta Carricondo Robino <65792055+Qk527@users.noreply.github.com>
2024-07-08 17:14:22 -0300

    Update abstraction.py

--- Bridge/abstraction.py
+++ Bridge/abstraction.py
@@ -2,4 +2,19 @@
 from implementor import Renderer
 
 class Shape:
-    
+    def __init__(self, renderer):
+        self.renderer = renderer
+
+    def draw(self):
+        pass
+
+    def resize(self, factor):
+        pass
+
+class Circle(Shape):
+    def __init__(self, renderer, radius):
+        super().__init__(renderer)
+        self.radius = radius
+
+    def draw(self):
+        self.renderer.render_shape("circle")
